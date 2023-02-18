96ba93fa9de8634ded71bc65363c858007a6f1e4
estudiandev <henrrybrgt@gmail.com>
2023-02-18 12:15:44 -0400

    added backgrounds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,17 +4,45 @@
 </script>
 
 <style>
+    *, *::before,*::after{
+        box-sizing: border-box;
+        margin: 0;
+    }
+
     .heather{
+        width: 100%;
+        height: 100vh;
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        justify-content: space-between;
+    }
+
+    .side1{
+        place-self: end left;
+        margin-left: 20px;
+        color: #fff;
+    }
+
+    .side1 .city{
+        text-align: left;
+    }
+
+    .side2{
         display: flex;
         flex-direction: column;
-        width: fit-content;
-        justify-content: center;
-        margin: 0 auto;
+        align-items: center;
+        justify-content: space-around;
+        backdrop-filter: blur(20px);
     }
+
     .country-group{
         display: flex;
         flex-direction: column;
+        flex-grow: 1.3;
+        margin-top: 20px;
+        gap: 10px;
     }
+
     h1, h2{
         text-align: center;
     }
@@ -25,38 +53,140 @@
         align-items: center;
     }
 
-    img{
-        height: 170px;
-    }
-
     .btn-city{
         background-color: #004c97;
         color: #fff;
+        border: 1px solid #fff;
+        border-radius: 7px;
+        outline: 0;
+    }
+
+    .inpts{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        gap: 4px;
+    }
+
+    #city-inp{
+        flex-grow: 1.8;
+        border: 1px solid white;
+        background-color: transparent;
+        padding: 5px;
+        color: #fff;
+        border-radius: 7px;
     }
 
     .heather p{
         margin: 2px;
     }
-</style>
-<h2>Henrry Weather</h2>
-{#await weatherPromise then weather}
-    <div class="heather">
-        <form class="country-group">
-            <label for="city-inp">Your City</label>
-            <div class="inpts">
-                <input class="form-input" type="text" placeholder="London" name="city-inp" id="city-inp" >
-                <button class="btn-city" type="submit" on:click={() => weather = searchWeatherCity(document.getElementById("city-inp").value)}>Go</button>
-            </div>
-        </form>
-        <div class="weather-condition">
-            <img src="{weather.icon}" alt="{weather.condition}">
-            <h1>{weather.temperature}°C</h1>
-        </div>
-        <h2>{weather.name}, {weather.country}</h2>
-        <p>Local Time: {weather.localtime}</p>
-        <p>{weather.condition}</p>
-        <p>Wind Direction: {weather.windDir}</p>
-        <p>Wind Speed: {weather.windSpeed}km/h</p>
-    </div>
 
+    .partly-cloudy,
+    .sunny,
+    .light-rain,
+    .clear,
+    .overcast,
+    .light-drizzle,
+    .fog,
+    .cloudy,.mist,
+    .patchy-rain-nearby{
+        background-position: center;
+        background-size: cover;
+        position: absolute;
+        color: #fff;
+        top: 0;
+        left: 0;
+    }
+
+    .fog,.mist{
+        background-image: url(https://cdn.pixabay.com/photo/2018/08/21/23/29/forest-3622519_960_720.jpg);
+    }
+
+    .fog .form-input, .fog .title, .mist .form-input, .mist .title{
+        color: #000;
+    }
+
+    .fog #city-inp, .fog .btn-city, .mist #city-inp, .mist .btn-city{
+        border-color: #000;
+    }
+
+    .light-drizzle{
+        background-image: url(https://cdn.pixabay.com/photo/2013/12/09/13/45/waterfall-225960_960_720.jpg);
+    }
+
+    .overcast{
+        background-image: url(https://cdn.pixabay.com/photo/2015/12/25/13/03/sky-1107579_960_720.jpg);
+    }
+
+    .clear{
+        background-image: url(https://cdn.pixabay.com/photo/2015/02/17/18/12/evening-lake-639699_960_720.jpg);
+    }
+    
+    .partly-cloudy, .cloudy{
+        background-image: url(https://cdn.pixabay.com/photo/2014/10/14/18/11/cloudscape-488478_960_720.jpg);
+    }
+
+    .light-rain, .patchy-rain-nearby{
+        background-image: url(https://cdn.pixabay.com/photo/2020/06/18/07/56/railing-5312344_960_720.jpg);
+    }
+
+    .sunny{
+        background-image: url(https://cdn.pixabay.com/photo/2018/02/05/23/05/death-valley-3133502_960_720.jpg);
+    }
+
+    .title{
+        position: absolute;
+        top: 5px;
+        left: 5px;
+        color: #ccc;
+    }
+
+    .temperature{
+        font-size: 100px;
+    }
+
+    .details{
+        margin: 10px 0;
+        border-bottom: 2px solid #fff;
+        text-align: left;
+    }
+
+    .another-info{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        flex-grow: 1.5;
+    }
+</style>
+{#await weatherPromise then weather}
+    <div class={`heather ${weather.condition.replace(/ /g, "-").toLowerCase()}`}>
+        <div class="side1">
+            <h2 class="title">Henrry Weather</h2>
+            <h2 class="city">{weather.name}, {weather.country}</h2>
+            <p class="localtime">Local Time: {weather.localtime}</p>
+            <p class="condition">{weather.condition}</p>
+            <h1 class="temperature">{weather.temperature}°C</h1>
+        </div>
+        <div class="side2">
+            <form class="country-group">
+                <div class="inpts">
+                    <input class="form-input" type="text" placeholder="Type the city" name="city-inp" id="city-inp" >
+                    <button class="btn-city" type="submit" on:click={() => weather = searchWeatherCity(document.getElementById("city-inp").value)}>Go</button>
+                </div>
+            </form>
+            <div class="another-info">
+                <div class="row1">
+                    <h2 class="details">Weather Details</h2>
+                    <p>Humidity: {weather.humidity}%</p>
+                    <p>Latitude: {weather.lat}</p>
+                    <p>Long: {weather.lon}</p>
+                </div>
+                <div class="row2">
+                    <h2 class="details">Wind Details</h2>
+                    <p>Wind Direction: {weather.windDir}</p>
+                    <p>Wind Speed: {weather.windSpeed}km/h</p>
+                </div>
+            </div>
+        </div>
+    </div>
 {/await}
--- src/routes/services/weather.js
+++ src/routes/services/weather.js
@@ -9,21 +9,29 @@ const options = {
 export let city = "Caracas"
 
 export async function searchWeatherCity(citySearch){
-    city = citySearch
-    const response = await fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${citySearch}&dt=2022-12-27`, options)
-    const responseJson = await response.json()
-    const {current, location} = responseJson
-    const {condition, temp_c, wind_dir, wind_kph} = current
-    const {text, icon} = condition
-    const {country, localtime, name} = location
-    return {
-        country,
-        localtime,
-        condition: text,
-        icon,
-        name,
-        temperature: temp_c,
-        windSpeed: wind_kph,
-        windDir: wind_dir
+    try {
+        city = citySearch
+        const response = await fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${citySearch}&dt=2022-12-27`, options)
+        const responseJson = await response.json()
+        const {current, location} = responseJson
+        const {condition, temp_c, wind_dir, wind_kph, humidity} = current
+        const {text} = condition
+        const {country, localtime, name, lat, lon} = location
+        return {
+            country,
+            localtime,
+            condition: text,
+            name,
+            temperature: temp_c,
+            windSpeed: wind_kph,
+            windDir: wind_dir,
+            humidity,
+            lat,
+            lon,
+
+        }
+    } catch (error) {
+        alert(`The city '${citySearch}' does not exists on the database`)
+        location.reload()
     }
 }
